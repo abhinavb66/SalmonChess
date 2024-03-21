816163b0add04f613b02825792d6dc50f1fdb624
abhinavb66 <abhinav_b@outlook.com>
2024-03-21 15:42:12 -0700

    Added UCI functionality

--- Salmon.py
+++ Salmon.py
@@ -1,5 +1,6 @@
 #Main python file for Salmon chess engine
 #Designed to interface through Universal Chess Interface (UCI) with a chess GUI
+#UCI protocol: https://www.wbec-ridderkerk.nl/html/UCIProtocol.html
 #This class handles communication with the GUI through the UCI protocol
 #Built by Abhinav Brahmarouthu
 
@@ -20,4 +21,37 @@ while True:
         print("id name Abhinav Brahmarouthu")
         print("uciok")
 
+    #end program if requested
+    elif(arg[0] == "quit"):
+        break
+
+    #respond to ready check
+    elif(arg[0] == "isready"):
+        return("readyok")
+    
+    #handle internal parameter change requests
+    elif(arg[0] == "setoption"):
+        if(arg[2] == "depth"):
+            depth = int(arg[4])
+
+    #Set up requested position
+    elif(arg[0] == "position"):
+        if(arg[1] == "startpos"):
+            board.reset()
+        elif(arg[1] == "fen"):
+            board.set_fen(" ".join(arg[2:8]))
+        else:
+            break #bad command
+        
+        start = arg.index("moves")
+        for move in arg[start+1:]:
+            board.uci_push(move)
+
+    #Start calculating current position
+    elif(arg[0] == "go"):
+        calc = 1
+
+    #Stop calculating current postition and return best move
+    elif(arg[0] == "stop"):
+        move = 1
     
